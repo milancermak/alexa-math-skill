b73d29fe3e578f6cc5783fb29dc69e65206770ba
Milan Cermak <milan.cermak@gmail.com>
2018-12-13 19:37:13 +0100

    Minor test env improvement

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,6 @@ os.environ['REGION'] = os.uname().nodename
 os.environ['SERVICE'] = 'alexa-math-skill'
 os.environ['STAGE'] = 'localtest'
 
-# https://github.com/spulec/moto/issues/1793
-# can go away once https://github.com/spulec/moto/pull/1952 is resolved
-os.environ['AWS_ACCESS_KEY_ID'] = 'test'
-os.environ['AWS_SECRET_ACCESS_KEY'] = 'test'
-
 # manipulating sys.path to make importing inside tests because ¯\_(ツ)_/¯
 here = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(here, '..', 'src')) # for core
