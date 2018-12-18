183f72137cf21ae47e8c838c6f8ffe5143ea760d
Milan Cermak <milan.cermak@gmail.com>
2018-12-18 19:02:41 +0100

    A more verbose lint_cfn

--- scripts/lint_cfn.sh
+++ scripts/lint_cfn.sh
@@ -9,4 +9,6 @@ EXIT_STATUS=0
 echo 'Linting CFN templates'
 find $INFRASTRUCTURE_DIR -name '*.yml' -print0 | xargs -0 -n1 cfn-lint --template || EXIT_STATUS=$?
 
+[ $EXIT_STATUS == 0 ] && echo -e 'All good\n'
+
 exit $EXIT_STATUS
